3f47fc06d332693d7795a825eec34121365b293a
mayaassem5 <mayaassem5@gmail.com>
2023-09-07 06:16:13 -0700

    sizeof1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@
 
 int main(void)
 {
-	puts("Size of a char:", sizeof(char));
-	puts("Size of an int:", sizeof(int));
-	puts("Size of a long int:", sizeof(int[10]));
-	puts("Size of a long long int:", sizeof(int[30]));
-	puts("Size of a float:", sizeof(float));
+	printf("Size of a char:", sizeof(char), "\n");
+	printf("Size of an int:", sizeof(int), "\n");
+	printf("Size of a long int:", sizeof(int[10]), "\n");
+	printf("Size of a long long int:", sizeof(int[30]), "\n");
+	printf("Size of a float:", sizeof(float), "\n");
 	return (0);
 }
