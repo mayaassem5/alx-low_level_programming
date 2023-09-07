7bc231248c13da2501ad7a5c1c94cb9c91424a49
mayaassem5 <mayaassem5@gmail.com>
2023-09-07 07:34:01 -0700

    quote2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -16,6 +16,8 @@ int main(void)
 	char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
 	ssize_t bytes_written = write(2, message, strlen(message));
 
+	writeln(2, message);
+
 	if (bytes_written == -1)
 	{
 		/* handle the error if write fails */
