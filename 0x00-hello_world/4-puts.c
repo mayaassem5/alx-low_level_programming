c95a8be6d91fe41c923e11be3f9cc9952a7446a6
mayaassem5 <mayaassem5@gmail.com>
2023-09-07 05:53:08 -0700

    first puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - puts "\"Programming is like building a multilingual puzzle"
+ *
+ * void: there's nothing here
+ *
+ * Return: 0 always
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
