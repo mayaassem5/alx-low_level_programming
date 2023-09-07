6254c947ae79407a2f5df84a4378ef4a1916167d
mayaassem5 <mayaassem5@gmail.com>
2023-09-07 06:01:16 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - printf "with proper grammar, but the outcome is a piece of art,"
+ *
+ *void: it has nothing in there
+ *
+ * Return: 0 always
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
