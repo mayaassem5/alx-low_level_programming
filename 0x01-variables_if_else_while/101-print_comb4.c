8ee33744b60354f18dc204bb4a6b274d28589b4f
mayaassem5 <mayaassem5@gmail.com>
2023-09-08 10:38:07 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ *main - entry point that prints digits
+ *
+ *void: has nothing
+ *
+ *Return: 0 always
+*/
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 1; y < 10; y++)
+		{
+			for (z = 2; z < 10; z++)
+			{
+				if (x == y || x == z || y == z)
+				{
+					continue;
+				}
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+
+				if (x == 7 && y == 8 && z == 9)
+				{
+					continue;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
