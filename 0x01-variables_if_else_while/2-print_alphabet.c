b67e02529ecaad6b1e5e669093ff477d5459ceb2
mayaassem5 <mayaassem5@gmail.com>
2023-09-08 05:25:13 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - puts "sizeof"
+ *
+ *void: has nothing
+ *
+ *Return: 0 always
+ *
+*/
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+}
