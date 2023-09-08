2632e33281fdf8e18402849892895a3a06bfaee2
mayaassem5 <mayaassem5@gmail.com>
2023-09-08 06:55:13 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *main - entry point prints digit numbers
+  *
+  *void: nothing here
+  *
+  *Return: 0 always
+  *
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	return (0);
+}
