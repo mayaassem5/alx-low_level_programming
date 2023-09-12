e0ab461fae827010b2d3fd875a11d5f6ae567722
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 10:18:16 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ *print_last_digit - prints an output
+ *
+ *@d: has value
+ *
+ *Return: 0 always
+*/
+
+int print_last_digit(int d)
+{
+	return (d % 10);
+}
