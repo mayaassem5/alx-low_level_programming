c305af4243b52b0433eff72e51a2d5ef4770e798
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 13:20:54 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *add - prints an output
+ *
+ *@i: has output
+ *@j: has output
+ *
+ *Return: 0 always
+*/
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}
