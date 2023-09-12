ab37a7e3165d99ce3a1ed54ab0d4004ad40dd813
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 09:12:33 -0700

    edit1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,26 +14,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		_putchar(',');
-		_putchar(' ');
 		_putchar(1 + '0');
 		_putchar('\n');
-	} else if (n == 0)
-	{
-		_putchar(48);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(0 + '0');
-		_putchar('\n');
-	} else
+	} else if (n < 0)
 	{
 		_putchar(45);
-		_putchar(',');
-		_putchar(' ');
 		_putchar(45);
 		_putchar(49);
 		_putchar('\n');
+	} else
+	{
+		_putchar(0 + '0');
+		return (0);
 	}
-	return (0);
 
 }
