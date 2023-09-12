2292032ec40cfc490007129ed6fb96ff9f5b884e
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 14:06:25 -0700

    table2

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *print_times_table - prints an output
+ *
+ *@n: has output
+ *
+ *Return: 0 always
+*/
+
+void print_times_table(int n)
+{
+	int a, b;
+
+	for (a = 0; b <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			if (n < 15 || n > 0)
+			{
+				c = a * b;
+
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (c < 10)
+					{
+						_putchar(' ');
+					}
+				}
+				if (c < 10)
+				{
+					_putchar(c + '0');
+				} else
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			} else
+			{
+				break;
+			}
+		}
+	}
+}
