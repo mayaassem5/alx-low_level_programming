1af536acbd2f7c15ba4b9cc853872f39706c8aa8
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 13:12:10 -0700

    table10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,15 +17,11 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c < 10)
+
+			if (b == 0)
 			{
-				_putchar(c + '0');
+				_putchar('0');
 			} else
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-			if (b < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -33,6 +29,14 @@ void times_table(void)
 				{
 					_putchar(' ');
 				}
+			}
+			if (c < 10)
+			{
+				_putchar(c + '0');
+			} else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
