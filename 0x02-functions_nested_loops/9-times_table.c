70ad2f7bba86a23c8334759ce3d4e790afc2cf8f
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 12:43:42 -0700

    table3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,8 +17,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			_putchar(c + '0');
-			_putchar((c % 10) + '0');
+			_putchar(c / 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
