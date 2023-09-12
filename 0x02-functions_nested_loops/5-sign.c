41396b24a2261b693d1c83019ee097195d43d0d5
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 09:08:27 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *print_sign - prints an output
+ *
+ *@n: has value
+ *
+ *Return: 0 always
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(1 + '0');
+		_putchar('\n');
+	} else if (n == 0)
+	{
+		_putchar(48);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(0 + '0');
+		_putchar('\n');
+	} else
+	{
+		_putchar(45);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(45);
+		_putchar(49);
+		_putchar('\n');
+	}
+	return (0);
+
+}
