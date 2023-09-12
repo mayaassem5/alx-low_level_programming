82970f1bcdb8aa235325a73be2c50f33ed00d400
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 14:18:06 -0700

    table6

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,30 +15,28 @@ void print_times_table(int n)
 
 	for (a = 0; b <= n; a++)
 	{
-		if (n < 15 || n > 0)
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
+			c = a * b;
+			if (b != 0)
 			{
-				c = a * b;
-				if (b != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (c < 10)
-					{
-						_putchar(' ');
-					}
-				}
+				_putchar(',');
+				_putchar(' ');
 				if (c < 10)
 				{
-					_putchar(c + '0');
-				} else
-				{
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
+					_putchar(' ');
 				}
 			}
-		} else
+			if (c < 10)
+			{
+				_putchar(c + '0');
+			} else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		if (n > 15 || n < 0)
 		{
 			break;
 		}
