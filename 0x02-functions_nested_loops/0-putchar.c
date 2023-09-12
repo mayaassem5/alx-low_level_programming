59053a3afa27f5e85a1cab3df5af14dd6d7f49a2
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 03:29:10 -0700

    adding main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 /**
- *main - print putchar
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
  *
- *void: return  nothing
- *
- *Return: 0 always
-*/
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ */
 
-int main(void)
+int _putchar(char c)
 {
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
