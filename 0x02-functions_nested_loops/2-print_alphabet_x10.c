43c461d7faf9dfaeece9ec7501199dcc27b78537
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 07:47:08 -0700

    alpha10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints an output
+ *
+ *void: has nothing
+ *
+ *Return: 0 always
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+
+		_putchar('\n');
+	}
+}
