90e3ad3ed7b53a398fa20b686b09498c5c92067c
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 02:11:43 -0700

    putchar3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  *main - print putchar
  *
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	write("_putchar\n");
+	_putchar('\n');
 	return (0);
 }
