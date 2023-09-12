b3c69961cd5090d70342cc8658373679711bf1e6
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 03:41:50 -0700

    edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,8 @@
 
 /**
  *main - writes the character c to stdout
- *@c: The character to print
+ *
+ *void: nothing here
  *
  *Return: On success 1.
  *On error, -1 is returned, and errno is set appropriately.
@@ -12,7 +13,7 @@
 
 int main(void)
 {
-	_putchar('a');
+	_putchar();
 	_putchar('\n');
 	return (0);
 }
