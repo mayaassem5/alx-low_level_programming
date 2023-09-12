8f7d53bdcad51bf5e390e11e53a3039344541ed7
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 09:40:00 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *_abs - prints an output
+ *
+ *@a: has value
+ *
+ *Return: 0 always
+*/
+
+int _abs(int a)
+{
+	_putchar(abs(a));
+	return (0);
+}
