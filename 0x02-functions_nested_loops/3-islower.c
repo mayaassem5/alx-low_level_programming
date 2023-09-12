2ca271f8ee3a906ce89f6a72bfd9a45a48fea8c0
mayaassem5 <mayaassem5@gmail.com>
2023-09-12 08:21:25 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *print_alphabet - prints an output
+ *
+ *void: has nothing
+ *
+ *Return: 0 always
+*/
+
+int _islower(int c)
+{
+	int r;
+
+	if (islower(c))
+	{
+		_putchar(r + '0');
+	}else
+	{
+		_putchar(r + '0');
+		_putchar('\n');
+		return (0);
+	}
+}
