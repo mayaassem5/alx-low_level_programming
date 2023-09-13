fc2cbe5433f4593ac2f02c0b1a9148d67f67c8ee
mayaassem5 <mayaassem5@gmail.com>
2023-09-13 06:02:18 -0700

    fibonacci3 2'

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,20 +10,43 @@
 */
 int main(void)
 {
-	long int a, b, c;
-	int d;
+		int c, boolean, boolean2;
+	long int n1, n2, fn, fn2, n11, n22;
 
-	a = 1;
-	b = 2;
-	printf("%ld, %ld", a, b);
-
-	for (d = 0; d < 98; d++)
+	n1 = 1;
+	n2 = 2;
+	boolean =  boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
-		c = a + b;
-		printf(", %ld", c);
-
-		a = b;
-		b = c;
+		if (boolean)
+		{
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = fn;
+		}
+		else
+		{
+			if (boolean2)
+			{
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
+		}
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
 	}
 	printf("\n");
 	return (0);
