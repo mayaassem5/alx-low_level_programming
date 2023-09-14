1a353df58f71190c93c1fa97673b1fb68cb94a98
mayaassem5 <mayaassem5@gmail.com>
2023-09-14 05:44:22 -0700

    edit10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,22 @@
 */
 int main(void)
 {
-	int i = 1;
+	int i;
 
+	i =  1;
 	printf("%d", i);
 
 	for (i = 2; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if (i % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
 		}
-		else if (((i % 3) == 0) && ((i % 5) == 0))
+		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		} else
