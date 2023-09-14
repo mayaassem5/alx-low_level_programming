03c99bec5cdef65b5c3190924941f7004d460e4f
mayaassem5 <mayaassem5@gmail.com>
2023-09-14 00:41:50 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ *mul - prints an output
+ *
+ *@a: has value
+ *@b: has value
+ *
+ *Return: 0 always
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
