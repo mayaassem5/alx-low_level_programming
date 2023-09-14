950c8bda10d0e0dc21cf9899b402a5a1b72ccd25
mayaassem5 <mayaassem5@gmail.com>
2023-09-14 00:49:33 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ *print_numbers - prints an output
+ *
+ *void: has value
+ *
+ *Return: 0 always
+*/
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+	return (0);
+}
