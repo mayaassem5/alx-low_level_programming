fa54029161fae9b83b821d997ab82412b3c205d4
mayaassem5 <mayaassem5@gmail.com>
2023-09-14 01:02:58 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ *print_most_numbers - prints an output
+ *
+ *void: has value
+ *
+ *Return: 0 always
+*/
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		if (num == 2 || num == 4)
+		{
+			continue;
+		} else
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
