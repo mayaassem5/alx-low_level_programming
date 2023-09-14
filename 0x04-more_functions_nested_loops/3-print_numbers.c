82d1dbdd640dbd85beeb3d7c51562173fc9ee933
mayaassem5 <mayaassem5@gmail.com>
2023-09-14 02:25:22 -0700

    print numbers3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,12 +12,11 @@
 */
 void print_numbers(void)
 {
-	int num = 0;
+	int num;
 
-	while (num < 10)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(num + '0');
-		num++;
 	}
 	_putchar('\n');
 	return (0);
