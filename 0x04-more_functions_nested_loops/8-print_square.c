3294d74cbd68cafc8c0153adb7d36f29f3b5ea0c
mayaassem5 <mayaassem5@gmail.com>
2023-09-14 04:20:25 -0700

    edit1

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,16 +14,14 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	for (i = 1; i <= size; i++)
 	{
-		for (i = 1; i <= size; i++)
+		for (j = 1; j <= size; j++)
 		{
-			for (j = 1; j <= size; j++)
-			{
+			if (size > 0)
 				_putchar('#');
-			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
