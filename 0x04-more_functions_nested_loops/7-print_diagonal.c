c208189f73c565378b5e438a9cf1da394b75b893
mayaassem5 <mayaassem5@gmail.com>
2023-09-14 04:17:48 -0700

    diagonal4

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,15 +14,15 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < i; j++)
 		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
+			_putchar(' ');
+		}
+		_putchar('\\');
+		if (i < (n - 1))
+		{
 			_putchar('\n');
 		}
 	}
