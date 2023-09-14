9ca5070b92178304c4fb508b6cbbc70b1e8e4e22
mayaassem5 <mayaassem5@gmail.com>
2023-09-13 23:53:24 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *_isupper - prints an output
+ *
+ *@c: has output
+ *
+ *Return: 0 always
+*/
+int _isupper(int c)
+{
+	if(int isupper(c))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
