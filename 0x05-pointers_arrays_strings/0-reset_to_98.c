4ad9d2c37dcb76ea36dfd51d6d1ff183e4a1804c
mayaassem5 <mayaassem5@gmail.com>
2023-09-18 06:49:32 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ *reset_to_98 - prints an output
+ *
+ *@n: has value
+ *
+ *Return: no always
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
