b7392257860e622864e84c721ded866c7f2434d3
mayaassem5 <mayaassem5@gmail.com>
2023-09-18 08:09:40 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+ */
+void rev_string(char *s)
+{
+	int i; 
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+}	
