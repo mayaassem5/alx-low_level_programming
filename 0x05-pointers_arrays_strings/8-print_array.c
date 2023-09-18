782972ad0ab80860dcfa32140cb949248d800fce
mayaassem5 <mayaassem5@gmail.com>
2023-09-18 10:04:30 -0700

    array2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,14 +13,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		_putchar(*(a + i));
+		printf("%d",*(a + i));
 		if ( (*(a + i)) != '0')
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf(", ");
 		}
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
 
