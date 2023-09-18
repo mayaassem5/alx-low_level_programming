da11ac86cdc2d3768dede0122cd401236713e124
mayaassem5 <mayaassem5@gmail.com>
2023-09-18 10:38:05 -0700

    atoi1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 /**
  * _atoi - converts a string to an integer.
  * @s: input string.
@@ -10,4 +11,6 @@ int _atoi(char *s)
 {
 	int a;
 	a = atoi(s);
+
+	return (0);
 }
