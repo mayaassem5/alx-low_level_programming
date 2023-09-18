1dba0d3ce4aea32075e5dfda7fa96ce76753c6f2
mayaassem5 <mayaassem5@gmail.com>
2023-09-18 10:48:19 -0700

    atoi4

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,29 @@
  */
 int _atoi(char *s)
 {
-	int a = atoi(s);
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	return (a);
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
