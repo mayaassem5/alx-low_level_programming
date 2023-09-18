041ca1868446005072ff5bc728d57f9e2c6e84e0
mayaassem5 <mayaassem5@gmail.com>
2023-09-18 10:13:10 -0700

    array6

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n - 1; i++)
 	{
-		printf("%d",*(a + i));
-		if ( (*(a + i)) != (*(a + n)))
+		printf("%d" , *(a + i));
+		if ((*(a + i)) != (*(a + n)))
 		{
 			printf(", ");
 		}
