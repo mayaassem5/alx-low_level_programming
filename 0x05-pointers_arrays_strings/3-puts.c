0f72e66169432931830ed76463defdd70451d714
mayaassem5 <mayaassem5@gmail.com>
2023-09-18 07:37:34 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ void swap_int(int *a, int *b);
 int _putchar(char c);
 void reset_to_98(int *n);
 int _strlen(char *s);
+void _puts(char *str);
