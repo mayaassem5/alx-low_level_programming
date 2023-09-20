b9e1fbd577f20c34e20f8c716e5d3ff795d7a7ab
mayaassem5 <mayaassem5@gmail.com>
2023-09-20 02:49:14 -0700

    strcmp and strncpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+}
