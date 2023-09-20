ab46ee35d0186c25f19bc83c544968ddd02d799a
mayaassem5 <mayaassem5@gmail.com>
2023-09-20 08:44:57 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
+	int i = 0, diff = 0;
+
+	while (diff == 0)
 	{
-		_putchar(15 + '0');
-	}
-	else if (s1 < s2)
-	{
-		_putchar(45);
-		_putchar(15 + '0');
-	}
-	else
-	{
-		_putchar(0);
+		if ((s1[i] == '\0') && (s2[i] == '\0'))
+			break;
+		diff = s1[i] - s2[i];
+		i++;
 	}
 
+	return (diff);
 }
