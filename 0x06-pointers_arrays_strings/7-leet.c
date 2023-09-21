4e594a5595613b1fb35c8628d1b6dc6c748e82f5
mayaassem5 <mayaassem5@gmail.com>
2023-09-21 05:31:43 -0700

    leet2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,26 +8,26 @@
 char *leet(char *s)
 {
 	int i;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'A' || s[i] == 'a')
+		if ((s[i] == 'A') || (s[i] == 'a'))
 		{
 			s[i] = 4;
 		}
-		else if (s[i] == 'E' || s[i] == 'e')
+		else if ((s[i] == 'E') || (s[i] == 'e'))
 		{
 			s[i] = 3;
 		}
-		else if (s[i] == 'O' || s[i] == 'o')
+		else if ((s[i] == 'O') || (s[i] == 'o'))
 		{
 			s[i] = 0;
 		}
-		else if (s[i] == 'T' || s[i] == 't')
+		else if ((s[i] == 'T') || (s[i] == 't'))
 		{
 			s[i] = 7;
 		}
-		else if (s[i] == 'L' || s[i] == 'l')
+		else if ((s[i] == 'L') || (s[i] == 'l'))
 		{
 			s[i] = 1;
 		}
