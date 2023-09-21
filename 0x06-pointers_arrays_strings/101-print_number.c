968f37115882a015a747227f5b96dbe29524bb41
mayaassem5 <mayaassem5@gmail.com>
2023-09-21 06:39:11 -0700

    int1

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,5 +6,20 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+
+	if (m / 10 != 0)
+		print_number(m / 10);
+
+	_putchar((m % 10) + '0');
 }
