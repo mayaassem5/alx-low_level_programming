83536bd38045f2a082853863b0c7f57976c663a3
mayaassem5 <mayaassem5@gmail.com>
2023-09-21 01:53:19 -0700

    cap2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 0, j;
 
 	char sub[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
@@ -17,9 +17,9 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		for (i = 0; i < 13; i++)
+		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == sub[i])
+			if (s[i] == sub[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 					s[i] = s[i] - 32;
