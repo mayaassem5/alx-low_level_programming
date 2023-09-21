e90353e591edd689a9f54c9800d036bf6bc58510
mayaassem5 <mayaassem5@gmail.com>
2023-09-21 05:37:34 -0700

    leet4

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,23 +13,23 @@ char *leet(char *s)
 	{
 		if ((s[i] == 'A') || (s[i] == 'a'))
 		{
-			s[i] = 4;
+			s[i] = '4';
 		}
 		else if ((s[i] == 'E') || (s[i] == 'e'))
 		{
-			s[i] = 3;
+			s[i] = '3';
 		}
 		else if ((s[i] == 'O') || (s[i] == 'o'))
 		{
-			s[i] = 0;
+			s[i] = '0';
 		}
 		else if ((s[i] == 'T') || (s[i] == 't'))
 		{
-			s[i] = 7;
+			s[i] = '7';
 		}
 		else if ((s[i] == 'L') || (s[i] == 'l'))
 		{
-			s[i] = 1;
+			s[i] = '1';
 		}
 		
 		i++;
