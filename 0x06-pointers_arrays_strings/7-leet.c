a88e91fb0c6b6b67fb81eefc05fbebbae937a05c
mayaassem5 <mayaassem5@gmail.com>
2023-09-21 05:25:20 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *leet(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 'A' || s[i] == 'a')
+		{
+			s[i] = 4;
+		}
+		else if (s[i] == 'E' || s[i] == 'e')
+		{
+			s[i] = 3;
+		}
+		else if (s[i] == 'O' || s[i] == 'o')
+		{
+			s[i] = 0;
+		}
+		else if (s[i] == 'T' || s[i] == 't')
+		{
+			s[i] = 7;
+		}
+		else if (s[i] == 'L' || s[i] == 'l')
+		{
+			s[i] = 1;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
+	}
+}
