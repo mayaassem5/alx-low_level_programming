af2524b9248f0d8ce128c614735ac72ab82bd55d
mayaassem5 <mayaassem5@gmail.com>
2023-09-25 05:38:44 -0700

    strspn2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,13 +10,14 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j;
 	unsigned int c = 0;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[j] == accept[i])
+			if (s[i] == accept[j])
 			{
 				c++;
+				break;
 			}
 		}
 	}
