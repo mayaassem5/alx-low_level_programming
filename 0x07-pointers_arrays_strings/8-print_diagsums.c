391e15d3fa11f4372b8399ed41763f5269d84ae5
mayaassem5 <mayaassem5@gmail.com>
2023-09-25 08:44:57 -0700

    diag1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,12 +13,11 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size * size; i++)
 	{
-		main += a[i];
+		if (i % (size + 1) == 0)
+			main += a[i];
 
 		if ((i % (size - 1) == 0) && (i != 0) && (i < size * size - 1))
-		{
 			sec += a[i];
-		}
 	}
 	printf("%d, %d\n", main, sec);
 }
