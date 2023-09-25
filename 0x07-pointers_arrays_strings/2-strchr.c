51973f8d9e56af1e9911c632ce34e973f687303b
mayaassem5 <mayaassem5@gmail.com>
2023-09-25 05:20:17 -0700

    strchr9

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (s[i]);
+			return (s);
 		}
 	}
 
 	return ('\0');
+
 }
