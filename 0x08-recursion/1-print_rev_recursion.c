a078e6e678d51de662a6487098828f899200e6af
mayaassem5 <mayaassem5@gmail.com>
2023-09-28 01:30:05 -0700

    rev1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == *(s + 0))
+	if (*s == '\0')
+	{	
 		_putchar(*s);
-
-	_putchar(_print_rev_recursion(s - 1));
-
+		_putchar(_print_rev_recursion(s + 1));
+	}
+	
+	_print_rev_recursion(s - 1);
 }
