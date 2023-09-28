b3df6667402b9d660cc79289c0b53f5d23dc0db8
mayaassem5 <mayaassem5@gmail.com>
2023-09-28 03:23:57 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n < 1)
+		return (0);
+	return (1);
+}
