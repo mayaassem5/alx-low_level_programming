37a4b7638d78d59704b328c3979e46d53db9e480
mayaassem5 <mayaassem5@gmail.com>
2023-09-28 01:53:43 -0700

    len4

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,6 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s == '\0')
-		return len;
-	len++;
-	_strlen_recursion(s + 1);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
 }
