d74ad82a17536df6b97ac92eada3025097152a04
mayaassem5 <mayaassem5@gmail.com>
2023-10-02 09:22:21 -0700

    mult3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,10 +11,13 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-		printf("%d\n", (argv[1] * argv[2]));
+		int mult = argv[1] * argv[2];
+
+		printf("%d\n", mult);
 	}
 	return (0);
 }
