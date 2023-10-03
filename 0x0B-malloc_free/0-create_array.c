1887acc60c349e7340c01f641bf6aeeee05cf5f6
mayaassem5 <mayaassem5@gmail.com>
2023-10-03 04:40:44 -0700

    edit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,16 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	int i;
 
+	if (size == 0)
+	{
+		return ('\0');
+	}
+
 	ptr = malloc(size * sizeof(char));
 
 	if (ptr == '\0')
 	{
-		return (0);
+		return ('\0');
 	}
 
 	i = 0;
