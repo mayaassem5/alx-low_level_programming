c5162ba2ae10720adad31225b567595f90b4a597
mayaassem5 <mayaassem5@gmail.com>
2023-10-04 08:47:28 -0700

    edit11

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,7 +12,7 @@ char *argstostr(int ac, char **av)
 {
 	int index = 0;
 	char *ptr;
-	int i = 0, len = 0, total_len = 0;
+	int i = 0, len = 0, total_len = 0, j = 0, ia = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
@@ -31,18 +31,24 @@ char *argstostr(int ac, char **av)
 	ptr = malloc(sizeof(char) * (total_len + 1));
 
 	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
 	{
-		while (av[i][len] != '\0')
-		{
-			ptr[index++] = av[i][len++];
-		}
-		ptr[index++] = '\n';
+		free(ptr);
+		return (NULL);
 	}
 
-	ptr[index] = '\0';
+	for (i = j = ia = 0; ia < total_len; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			ptr[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < total_len - 1)
+			ptr[ia] = av[i][j];
+	}
+	ptr[ia] = '\0';
 
 
 	return (ptr);
