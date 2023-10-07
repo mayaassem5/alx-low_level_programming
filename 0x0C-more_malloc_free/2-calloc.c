47707bd64fc3467b8acf57f55c2d5147c882ac12
mayaassem5 <mayaassem5@gmail.com>
2023-10-07 08:43:48 -0700

    edit3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,13 +15,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(size * nmemb);
 
-	*ptr = 0;
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	if (ptr == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
 	return (ptr);
 }
