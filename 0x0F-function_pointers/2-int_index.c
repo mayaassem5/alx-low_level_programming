8471cfa060925f9d70b8239cf6bfb03f8ee7d58c
mayaassem5 <mayaassem5@gmail.com>
2023-10-11 06:21:00 -0700

    edit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,15 +8,18 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i;
 
-	if (size <= 0 || !(array) || !(cmp))
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
 
 	return (-1);
