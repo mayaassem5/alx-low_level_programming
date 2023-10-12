6e8904d942fa44a0ca704c74a1efff784f94d18c
mayaassem5 <mayaassem5@gmail.com>
2023-10-12 13:59:31 -0700

    edit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,48 +9,43 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
+	int i = 0;
+	char *str, *sep = "";
 
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	va_list list;
 
-	va_start(args, format);
+	va_start(list, format);
 
-	while (format && format[i])
+	if (format)
 	{
-		j = 0;
-		while (t_arg[j])
+		while (format[i])
 		{
-			if (format[i] == t_arg[j] && c)
+			switch (format[i])
 			{
-				printf(", ");
-				break;
-			} j++;
-		}
-
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(args, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(args, char *), c = 1;
-			if (str == NULL)
-			{
-				printf("(nil)");
-				break;
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
-
-			printf("%s", str);
-			break;
-		} i++;
+			sep = ", ";
+			i++;
+		}
 	}
-	printf("\n"), va_end(args);
+
+	printf("\n");
+	va_end(list);
 }
