ebef96a5be7e352e61b9c3b6a98236239e2ad841
mayaassem5 <mayaassem5@gmail.com>
2023-12-08 15:28:09 +0200

    edit betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *  * free_dlistint - frees a dlistint_t list
- *   *
- *    * @head: head of the list
- *     * Return: no return
- *      */
+ ** free_dlistint - frees a dlistint_t list
+ **
+ ** @head: head of the list
+ ** Return: no return
+ **/
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
