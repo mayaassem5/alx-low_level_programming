bb59f1652b6a04d72cbc2f9b6478333741b0a1ef
mayaassem5 <mayaassem5@gmail.com>
2023-12-08 16:08:32 +0200

    edit name

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,10 +21,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (head != NULL)
 	{
-		if (i == index)
+		if (num == index)
 			break;
 		head = head->next;
-		i++;
+		num++;
 	}
 	return (head);
 }
